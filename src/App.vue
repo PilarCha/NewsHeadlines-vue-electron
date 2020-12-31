a718921400e38d57497ec0c04958403264507b66
PilarCha <pchavez92@gmail.com>
2020-12-30 21:04:08 -0800

    Header page complete for now. Dun dun duuuuun

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <app-header />
+    <router-view />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import appHeader from '@/components/Header.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    appHeader
   }
 }
 </script>
---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <header class = 'header'>
+    <div class="logo">
+      <div class="logo_container">
+        <img src="../assets/logo.png" alt="New app Logo" class= "logo_image">
+      </div>
+      <h1>News App</h1>
+    </div>
+    <nav class = "nav">
+      <h4 class = "nav_link">
+        <router-link to="/home">Home</router-link>
+      </h4>
+      <h4 class = "nav_link">
+        <router-link to="/top-news">Top News</router-link>
+      </h4>
+      <h4 class = "nav_link">
+        <router-link to="/categories">News By Category</router-link>
+      </h4>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name:"app-header",
+}
+</script>
+
+<style lang="css" scoped>
+</style>
