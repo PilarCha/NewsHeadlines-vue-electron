b7031ff517b56684a9ca5f3a0808c5b9f19c8b0a
PilarCha <pchavez92@gmail.com>
2020-12-30 23:34:02 -0800

    started home page. continue here

---
+++ src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class = "home">
+    <h1>Welcome to News App</h1>
+    <h4>Displaying Top News from {{ countryInfo.name }}</h4>
+
+  </section>
+
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
