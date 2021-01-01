2816b4db5bd1a6a191a849aa78a3269c251dcbf5
PilarCha <pchavez92@gmail.com>
2020-12-31 19:29:10 -0800

    finished up the Home vue page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,15 +2,58 @@
   <section class = "home">
     <h1>Welcome to News App</h1>
     <h4>Displaying Top News from {{ countryInfo.name }}</h4>
-
+    <div class="articles_div" v-if = "articles">
+      <news-card
+        v-for="(article,idx) in articles"
+        :key="idx"
+        :article="article"
+      ></news-card>
+    </div>
   </section>
-
 </template>
 
 <script>
+  import { mapActions, mapState } from "vuex";
+  import NewsCard from "../components/NewsCard";
+
 export default {
-}
+  data() {
+    return {
+      articles: "",
+      countryInfo: "",
+    };
+  };
+  components: {
+    NewsCard,
+  },
+  mounted() {
+    this.fetchTopNews();
+  },
+  computed: {
+    ...mapState(["countries"]),
+  },
+  methods: {
+    ...mapActions(["getTopNews"]),
+    async fetchTopNews() {
+      let countriesLength = this.countries.length;
+      let countryIdx = Math.floor(
+        Math.random() * (countriesLength - 1) + 1
+      );
+      this.countryInfor = this.countries[countryIdx];
+      let { data } = await this.getTopNews(
+        this.countries[countryIdx].value
+      );
+      this.articles = data.articles;
+    },
+  },
+};
 </script>
 
 <style lang="css">
+  .articles_div {
+    display:flex;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+
 </style>
