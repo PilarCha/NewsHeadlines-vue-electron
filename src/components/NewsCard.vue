5d89e427ad3f0cab706eac0f4cbb7a2802a94386
PilarCha <pchavez92@gmail.com>
2021-01-01 19:58:32 -0800

    News card finishing up the methods

--- src/components/NewsCard.vue
+++ src/components/NewsCard.vue
@@ -10,12 +10,37 @@
       <p class="article_paragraph"> {{ article.description }}</p>
       <h5 class="article_published"> {{ new Date(article.publishedAt) }} </h5>
     </div>
-
+    <div class="image_container">
+      <img
+        class="news_img"
+        src="../assets/logo.png"
+        :data-src="article.urlToImage"
+        :alt="article.title"
+      />
+    </div>
   </section>
 </template>
 
 <script>
 export default {
+  name: "news-card",
+  props: {
+    article:Object,
+  },
+  mounted() {
+    this.lazyLoadImages();
+  },
+  methods: {
+    lazyLoadImages() {
+      const images = document.querySelectorAll(".news_img");
+      const options = {
+        // image within 50px in the Y axis. Download it baby
+        root: null,
+        rootMargin: "0px",
+        threshold: 0.1,
+      };
+    }
+  }
 }
 </script>
 
