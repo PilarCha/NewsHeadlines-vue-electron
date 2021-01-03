5b3f79a1dda94e4dec76763f3926a1ebea2064d0
PilarCha <pchavez92@gmail.com>
2021-01-03 13:31:55 -0800

    setup the store to receive news. Setting up a router next

--- src/store.js
+++ src/store.js
@@ -4,6 +4,44 @@ import axios from '../plugins/axios';
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
-
+const store = new Vuex.Store({
+  state: {
+    countries:[{
+      name: "United States of America",
+      value: "us",
+    },
+    {
+      name: "Nigeria",
+      value: "ng",
+    },
+    {
+      name: "Argentina",
+      value: "ar",
+    },
+    {
+      name: "Canada",
+      value: "ca",
+    },
+    {
+      name: "Sounth Africa",
+      value: "za",
+    },
+  ],
+    categories: [
+      "health",
+      "science",
+      "sports",
+      "technology",
+    ],
+  },
+  mutations: {},
+  actions: {
+    async getTopNews(context, country) {
+      let res = await axios({
+        url: `/top-headlines?country=${country}`,
+        method: "GET",
+      });
+    },
+  },
 });
+export default store;
