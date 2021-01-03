c8de825def76b487881b425418a079cd74c71834
PilarCha <pchavez92@gmail.com>
2021-01-03 13:03:33 -0800

    finished up the styles for News Card page

--- src/components/NewsCard.vue
+++ src/components/NewsCard.vue
@@ -70,5 +70,54 @@ export default {
 };
 </script>
 
-<style lang="css">
+<style>
+  .news {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    max-width: 550px;
+    box-shadow: 2px 1px 7px 1px #eee;
+    padding: 20px 5px;
+    box-sizing: border-box;
+    margin: 15px 5px;
+    border-radius: 4px;
+  }
+  .news_section {
+    width: 100%;
+    max-width: 350px;
+    margin-right: 5px;
+  }
+  .news_title {
+    font-size: 15px;
+    text-align: left;
+    font-weight: normal;
+  }
+  .news_author {
+    font-size: 14px;
+    text-align: left;
+    font-weight: normal;
+  }
+  .article_published {
+    text-align: left;
+  }
+  .image_container {
+    width: 100%;
+    max-width: 180px;
+    max-height: 180px;
+  }
+  .news_img {
+    transition: max_width 300ms cubic-bezier(0.4, 0, 1, 1),
+      max-height 300ms cubic-bezier(0.4, 0, 1, 1);
+    max-width: 150px;
+    max-height: 150px;
+  }
+  .new_img:hover {
+    max-width: 180px;
+    max-height: 180px;
+  }
+  .article_link {
+    text-decoration: none;
+    color: inherit;
+  }
 </style>
